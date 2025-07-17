0762e0a13edc9139db6442969d2af3ab58f25902
Mohit Ganna <mohitganna@gmail.com>
2025-07-17 11:31:18 -0400

    Added component library primereact

--- sageinventory/src/app/layout.tsx
+++ sageinventory/src/app/layout.tsx
@@ -2,6 +2,12 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+import "primeflex/primeflex.css";
+import "primeicons/primeicons.css";
+
+import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -23,10 +29,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <PrimeReactProvider>
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
     </html>
+    </PrimeReactProvider>
   );
 }
